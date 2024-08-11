f9c8b39ce8aa2efc5a8eb5169090b277b92884e3
rafailpapastamou <raphaelpapast@gmail.com>
2024-08-12 01:57:11 +0300

    added app.app_context so the database creation is performed within an app context

--- app.py
+++ app.py
@@ -239,6 +239,7 @@ def get_playlist_id(sp, user_id, playlist_prefix='My Monthly Top Tracks'):
     return None
 
 if __name__ == "__main__":
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
