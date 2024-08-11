040d6b46d6c2daff04f665f46ab53ea360b89690
rafailpapastamou <raphaelpapast@gmail.com>
2024-08-11 16:21:07 +0300

    changed the way the token is being passed

--- app.py
+++ app.py
@@ -65,8 +65,9 @@ def callback():
 
     # Create JWT and return it to the client
     token = create_jwt(user_id)
-    # return jsonify({'token': token, 'access_token': token_info['access_token']})
-    return redirect(url_for('create_or_update_playlist', token=token))
+    # Ensure the token is URL-encoded
+    encoded_token = jwt.utils.base64url_encode(token.encode()).decode()
+    return redirect(url_for('create_or_update_playlist', token=encoded_token))
 
 # Middleware to protect routes
 def token_required(f):
