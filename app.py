2439748a3149b07927adb3ca27c2b7080e79d74d
rafailpapastamou <raphaelpapast@gmail.com>
2024-08-12 21:32:22 +0300

    added 'session' to the imports

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, request, render_template, jsonify
+from flask import Flask, redirect, url_for, request, render_template, jsonify, session
 import spotipy
 import os
 import datetime
